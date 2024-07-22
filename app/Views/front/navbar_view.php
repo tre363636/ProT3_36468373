c9d9da81c8430b73f2f8a4ea4d635f49cba3ddc4
javi <trexenlineajavier36@gmail.com>
2024-07-22 17:42:19 -0300

    subida3

--- app/Views/front/navbar_view.php
+++ app/Views/front/navbar_view.php
@@ -1,15 +1,26 @@
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light custom-navbar">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="<?php echo base_url ('principal')?>">Principal</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo base_url ('quienes_somos')?>">Quiénes Somos</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo base_url ('acerca_de')?>">Acerca De</a></li>
-                </ul>
-            </div>
+<?php
+$current_uri = service('uri')->getSegment(1); // Obtén el primer segmento de la URI
+?>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link <?php echo ($current_uri == '') ? 'active' : ''; ?>" href="<?php echo base_url(); ?>">Principal</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php echo ($current_uri == 'quienes_somos') ? 'active' : ''; ?>" href="<?php echo base_url('quienes_somos'); ?>">Quiénes Somos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php echo ($current_uri == 'acerca_de') ? 'active' : ''; ?>" href="<?php echo base_url('acerca_de'); ?>">Acerca De</a>
+                </li>
+                <!-- Añade más enlaces según sea necesario -->
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
