7f7ff66c7129ac0d31a514d90debdcda729e2c57
javi <trexenlineajavier36@gmail.com>
2024-07-23 21:00:05 -0300

    arreglos menores2

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'mapache',
+        'database'     => 'miranda_javier',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
